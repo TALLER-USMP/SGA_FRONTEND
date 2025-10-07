c46108478e3fb1c8300e9c6e28a05ca272e39da9
GeraYF <geraldine_yanque@usmp.pe>
2025-10-07 09:39:23 -0500

    style: levantar observaciones del maquetado paso 4

--- apps/dashboard/src/components/layout/Layout.tsx
+++ apps/dashboard/src/components/layout/Layout.tsx
@@ -9,11 +9,11 @@ export default function Layout({
   title: string;
 }) {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen overflow-hidden">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col max-h-screen">
         <Header title={title} />
-        <main className="p-6 bg-white flex-1">{children}</main>
+        <main className="p-6 bg-white flex-1 overflow-auto">{children}</main>
       </div>
     </div>
   );
