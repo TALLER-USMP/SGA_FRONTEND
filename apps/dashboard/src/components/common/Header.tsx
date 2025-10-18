690ff4dfc48cdc7fa0032f988cf9a3803cead56f
leonardoPBF <leonardobf1402224@gmail.com>
2025-10-18 00:52:30 -0500

    fix: call for backend /me

--- apps/dashboard/src/components/common/Header.tsx
+++ apps/dashboard/src/components/common/Header.tsx
@@ -1,14 +1,11 @@
-interface HeaderProps {
-  user?: {
-    name?: string;
-  };
-}
-
+import type HeaderProps from "../../interfaces/headerProps";
 export default function Header({ user }: HeaderProps) {
+  const displayName = user?.name || user?.email || "Docente";
+
   return (
     <header className="w-full bg-red-700 text-white flex justify-end items-center px-6 py-3">
       <button className="bg-white text-black px-4 py-1 rounded shadow">
-        {user?.name ? `${user.name} ⬇` : "Docente ⬇"}
+        {`${displayName} ⬇`}
       </button>
     </header>
   );
